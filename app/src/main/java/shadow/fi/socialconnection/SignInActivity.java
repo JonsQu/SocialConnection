e1d358193d46b1c5a2babe552a57ccf57dc19cc4
JonsQu <joonatan.haavisto@cs.tamk.fi>
2018-04-12 20:26:28 +0300

    Modify: SignInActivity, added onCreate Refactor: signin.xml to activity_signin.xml

--- app/src/main/java/shadow/fi/socialconnection/SignInActivity.java
+++ app/src/main/java/shadow/fi/socialconnection/SignInActivity.java
@@ -1,4 +1,11 @@
 package shadow.fi.socialconnection;
 
+import android.os.Bundle;
+
 public class SignInActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
 }
