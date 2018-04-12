810db0740311aed750ced61c103bfe4a4ab360d3
JonsQu <joonatan.haavisto@cs.tamk.fi>
2018-04-12 20:30:46 +0300

    Modify: SignInActivity, added saveUserInfo method

--- app/src/main/java/shadow/fi/socialconnection/SignInActivity.java
+++ app/src/main/java/shadow/fi/socialconnection/SignInActivity.java
@@ -1,11 +1,16 @@
 package shadow.fi.socialconnection;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInActivity extends BaseActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+    }
+
+    public void saveUserInfo(View view) {
+
     }
 }
